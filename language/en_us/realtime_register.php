33a4df401e34075fdc284483281ec59c74eb4a6b
Abdy Franco <abdy@devmail.one>
2025-03-25 16:50:33 -0600

    Initial Commit

---
+++ language/en_us/realtime_register.php
@@ -0,0 +1,278 @@
+<?php
+/**
+ * en_us language for the Realtime Register module.
+ */
+// Basics
+$lang['RealtimeRegister.name'] = ' Realtime Register';
+$lang['RealtimeRegister.description'] = 'We offer 2,000+ TLDs from over 150 registries, and are constantly adding new ones. We take care of registry updates and all policies and procedures.';
+$lang['RealtimeRegister.module_row'] = 'Account';
+$lang['RealtimeRegister.module_row_plural'] = 'Accounts';
+$lang['RealtimeRegister.module_group'] = 'Account Group';
+
+
+// Module management
+$lang['RealtimeRegister.add_module_row'] = 'Add Account';
+$lang['RealtimeRegister.add_module_group'] = 'Add Account Group';
+$lang['RealtimeRegister.manage.module_rows_title'] = 'Accounts';
+
+$lang['RealtimeRegister.manage.module_rows_heading.customer'] = 'Customer';
+$lang['RealtimeRegister.manage.module_rows_heading.api_key'] = 'API Key';
+$lang['RealtimeRegister.manage.module_rows_heading.options'] = 'Options';
+$lang['RealtimeRegister.manage.module_rows.edit'] = 'Edit';
+$lang['RealtimeRegister.manage.module_rows.delete'] = 'Delete';
+$lang['RealtimeRegister.manage.module_rows.confirm_delete'] = 'Are you sure you want to delete this Account';
+
+$lang['RealtimeRegister.manage.module_rows_no_results'] = 'There are no Accounts.';
+
+$lang['RealtimeRegister.manage.module_groups_title'] = 'Groups';
+$lang['RealtimeRegister.manage.module_groups_heading.name'] = 'Name';
+$lang['RealtimeRegister.manage.module_groups_heading.module_rows'] = 'Accounts';
+$lang['RealtimeRegister.manage.module_groups_heading.options'] = 'Options';
+
+$lang['RealtimeRegister.manage.module_groups.edit'] = 'Edit';
+$lang['RealtimeRegister.manage.module_groups.delete'] = 'Delete';
+$lang['RealtimeRegister.manage.module_groups.confirm_delete'] = 'Are you sure you want to delete this Account';
+
+$lang['RealtimeRegister.manage.module_groups.no_results'] = 'There is no Account Group';
+
+
+// Add row
+$lang['RealtimeRegister.add_row.box_title'] = ' Realtime Register - Add Account';
+$lang['RealtimeRegister.add_row.add_btn'] = 'Add Account';
+
+
+// Edit row
+$lang['RealtimeRegister.edit_row.box_title'] = ' Realtime Register - Edit Account';
+$lang['RealtimeRegister.edit_row.edit_btn'] = 'Update Account';
+
+
+// Row meta
+$lang['RealtimeRegister.row_meta.customer'] = 'Customer';
+$lang['RealtimeRegister.row_meta.api_key'] = 'API Key';
+$lang['RealtimeRegister.row_meta.sandbox'] = 'Sandbox';
+
+
+// Errors
+$lang['RealtimeRegister.!error.customer.empty'] = 'Please enter a customer handle';
+$lang['RealtimeRegister.!error.api_key.valid'] = 'Invalid API Key';
+$lang['RealtimeRegister.!error.api_key.valid_connection'] = 'Unable to make a connection using the given credentials.';
+$lang['RealtimeRegister.!error.sandbox.format'] = 'Sandbox must be either "true" or "false".';
+$lang['RealtimeRegister.!error.module_row.missing'] = 'An internal error occurred. The module row is unavailable.';
+
+
+// Service info
+$lang['RealtimeRegister.service_info.domain'] = 'Domain';
+
+
+// Service Fields
+$lang['RealtimeRegister.service_fields.domain'] = 'Domain';
+$lang['RealtimeRegister.service_fields.ns1'] = 'Name Server 1';
+$lang['RealtimeRegister.service_fields.ns2'] = 'Name Server 2';
+$lang['RealtimeRegister.service_fields.ns3'] = 'Name Server 3';
+$lang['RealtimeRegister.service_fields.ns4'] = 'Name Server 4';
+$lang['RealtimeRegister.service_fields.ns5'] = 'Name Server 5';
+
+
+// Package Fields
+$lang['RealtimeRegister.package_fields.epp_code'] = 'EPP Code';
+
+$lang['RealtimeRegister.package_field.tooltip.epp_code'] = 'Whether to allow users to request an EPP Code through the Blesta service interface.';
+$lang['RealtimeRegister.package_fields.tld_options'] = 'TLDs';
+
+
+// Tab contacts
+$lang['RealtimeRegister.tab_contacts.title'] = 'Contacts';
+$lang['RealtimeRegister.tab_contacts.section_ADMIN'] = 'Administrative';
+$lang['RealtimeRegister.tab_contacts.section_BILLING'] = 'Billing';
+$lang['RealtimeRegister.tab_contacts.section_TECH'] = 'Technical';
+
+$lang['RealtimeRegister.tab_contacts.field_email'] = 'Email Address';
+$lang['RealtimeRegister.tab_contacts.field_phone'] = 'Phone Number';
+$lang['RealtimeRegister.tab_contacts.field_first_name'] = 'First Name';
+$lang['RealtimeRegister.tab_contacts.field_last_name'] = 'Last Name';
+$lang['RealtimeRegister.tab_contacts.field_address1'] = 'Address Line 1';
+$lang['RealtimeRegister.tab_contacts.field_address2'] = 'Address Line 2';
+$lang['RealtimeRegister.tab_contacts.field_city'] = 'City';
+$lang['RealtimeRegister.tab_contacts.field_state'] = 'State';
+$lang['RealtimeRegister.tab_contacts.field_zip'] = 'Zip';
+$lang['RealtimeRegister.tab_contacts.field_country'] = 'Country';
+
+$lang['RealtimeRegister.tab_contacts.field_update'] = 'Update Contacts';
+
+
+// Tab client contacts
+$lang['RealtimeRegister.tab_client_contacts.title'] = 'Contacts';
+$lang['RealtimeRegister.tab_client_contacts.section_ADMIN'] = 'Administrative';
+$lang['RealtimeRegister.tab_client_contacts.section_BILLING'] = 'Billing';
+$lang['RealtimeRegister.tab_client_contacts.section_TECH'] = 'Technical';
+
+$lang['RealtimeRegister.tab_client_contacts.field_email'] = 'Email Address';
+$lang['RealtimeRegister.tab_client_contacts.field_phone'] = 'Phone Number';
+$lang['RealtimeRegister.tab_client_contacts.field_first_name'] = 'First Name';
+$lang['RealtimeRegister.tab_client_contacts.field_last_name'] = 'Last Name';
+$lang['RealtimeRegister.tab_client_contacts.field_address1'] = 'Address Line 1';
+$lang['RealtimeRegister.tab_client_contacts.field_address2'] = 'Address Line 2';
+$lang['RealtimeRegister.tab_client_contacts.field_city'] = 'City';
+$lang['RealtimeRegister.tab_client_contacts.field_state'] = 'State';
+$lang['RealtimeRegister.tab_client_contacts.field_zip'] = 'Zip';
+$lang['RealtimeRegister.tab_client_contacts.field_country'] = 'Country';
+
+$lang['RealtimeRegister.tab_client_contacts.field_update'] = 'Update Contacts';
+
+
+// Tab nameservers
+$lang['RealtimeRegister.tab_nameservers.title'] = 'Nameservers';
+$lang['RealtimeRegister.tab_nameservers.heading'] = 'Nameservers';
+$lang['RealtimeRegister.tab_nameservers.field_update'] = 'Update';
+
+
+// Tab client nameservers
+$lang['RealtimeRegister.tab_client_nameservers.title'] = 'Nameservers';
+$lang['RealtimeRegister.tab_client_nameservers.heading'] = 'Nameservers';
+$lang['RealtimeRegister.tab_client_nameservers.field_update'] = 'Update';
+
+
+// Tab hosts
+$lang['RealtimeRegister.tab_hosts.title'] = 'Hosts';
+$lang['RealtimeRegister.tab_hosts.heading'] = 'Hosts';
+$lang['RealtimeRegister.tab_hosts.heading_host'] = 'Host';
+$lang['RealtimeRegister.tab_hosts.heading_ip_address'] = 'IP Address';
+$lang['RealtimeRegister.tab_hosts.heading_options'] = 'Options';
+$lang['RealtimeRegister.tab_hosts.heading_add'] = 'Add Host';
+$lang['RealtimeRegister.tab_hosts.text_no_hosts'] = 'There are currently no hosts for this domain.';
+$lang['RealtimeRegister.tab_hosts.field_delete'] = 'Delete';
+$lang['RealtimeRegister.tab_hosts.field_host'] = 'Host';
+$lang['RealtimeRegister.tab_hosts.field_ip_address'] = 'IP Address';
+$lang['RealtimeRegister.tab_hosts.field_add'] = 'Add Host';
+
+
+// Tab client hosts
+$lang['RealtimeRegister.tab_client_hosts.title'] = 'Hosts';
+$lang['RealtimeRegister.tab_client_hosts.heading'] = 'Hosts';
+$lang['RealtimeRegister.tab_client_hosts.heading_host'] = 'Host';
+$lang['RealtimeRegister.tab_client_hosts.heading_ip_address'] = 'IP Address';
+$lang['RealtimeRegister.tab_client_hosts.heading_options'] = 'Options';
+$lang['RealtimeRegister.tab_client_hosts.heading_add'] = 'Add Host';
+$lang['RealtimeRegister.tab_client_hosts.text_no_hosts'] = 'There are currently no hosts for this domain.';
+$lang['RealtimeRegister.tab_client_hosts.field_delete'] = 'Delete';
+$lang['RealtimeRegister.tab_client_hosts.field_host'] = 'Host';
+$lang['RealtimeRegister.tab_client_hosts.field_ip_address'] = 'IP Address';
+$lang['RealtimeRegister.tab_client_hosts.field_add'] = 'Add Host';
+
+
+// Tab DNSSEC
+$lang['RealtimeRegister.tab_dnssec.title'] = 'DNSSEC';
+$lang['RealtimeRegister.tab_dnssec.heading'] = 'DNSSEC Keys';
+$lang['RealtimeRegister.tab_dnssec.heading_add_dnssec'] = 'Add DNSSEC Key';
+$lang['RealtimeRegister.tab_dnssec.heading_dnssec_digest'] = 'DNSSEC Digest';
+$lang['RealtimeRegister.tab_dnssec.heading_protocol'] = 'Protocol';
+$lang['RealtimeRegister.tab_dnssec.heading_flags'] = 'Flags';
+$lang['RealtimeRegister.tab_dnssec.heading_algorithm'] = 'Algorithm';
+$lang['RealtimeRegister.tab_dnssec.heading_public_key'] = 'Public Key';
+$lang['RealtimeRegister.tab_dnssec.heading_options'] = 'Options';
+$lang['RealtimeRegister.tab_dnssec.field_delete'] = 'Delete';
+$lang['RealtimeRegister.tab_dnssec.field_protocol'] = 'Protocol';
+$lang['RealtimeRegister.tab_dnssec.field_flags'] = 'Flags';
+$lang['RealtimeRegister.tab_dnssec.field_algorithm'] = 'Algorithm';
+$lang['RealtimeRegister.tab_dnssec.field_publicKey'] = 'Public Key';
+$lang['RealtimeRegister.tab_dnssec.field_add'] = 'Add Key';
+$lang['RealtimeRegister.tab_dnssec.text_no_dnssec_keys'] = 'There are currently no DNSSEC Keys for this domain.';
+
+
+// Tab client DNSSEC
+$lang['RealtimeRegister.tab_client_dnssec.title'] = 'DNSSEC';
+$lang['RealtimeRegister.tab_client_dnssec.heading'] = 'DNSSEC Keys';
+$lang['RealtimeRegister.tab_client_dnssec.heading_add_dnssec'] = 'Add DNSSEC Key';
+$lang['RealtimeRegister.tab_client_dnssec.heading_dnssec_digest'] = 'DNSSEC Digest';
+$lang['RealtimeRegister.tab_client_dnssec.heading_protocol'] = 'Protocol';
+$lang['RealtimeRegister.tab_client_dnssec.heading_flags'] = 'Flags';
+$lang['RealtimeRegister.tab_client_dnssec.heading_algorithm'] = 'Algorithm';
+$lang['RealtimeRegister.tab_client_dnssec.heading_public_key'] = 'Public Key';
+$lang['RealtimeRegister.tab_client_dnssec.heading_options'] = 'Options';
+$lang['RealtimeRegister.tab_client_dnssec.field_delete'] = 'Delete';
+$lang['RealtimeRegister.tab_client_dnssec.field_protocol'] = 'Protocol';
+$lang['RealtimeRegister.tab_client_dnssec.field_flags'] = 'Flags';
+$lang['RealtimeRegister.tab_client_dnssec.field_algorithm'] = 'Algorithm';
+$lang['RealtimeRegister.tab_client_dnssec.field_publicKey'] = 'Public Key';
+$lang['RealtimeRegister.tab_client_dnssec.field_add'] = 'Add Key';
+$lang['RealtimeRegister.tab_client_dnssec.text_no_dnssec_keys'] = 'There are currently no DNSSEC Keys for this domain.';
+
+
+// Tab DNS
+$lang['RealtimeRegister.tab_dns.title'] = 'DNS Zone';
+$lang['RealtimeRegister.tab_dns.heading'] = 'DNS Records';
+$lang['RealtimeRegister.tab_dns.heading_add_record'] = 'Add DNS Record';
+$lang['RealtimeRegister.tab_dns.heading_name'] = 'Name';
+$lang['RealtimeRegister.tab_dns.heading_type'] = 'Type';
+$lang['RealtimeRegister.tab_dns.heading_content'] = 'Content';
+$lang['RealtimeRegister.tab_dns.heading_ttl'] = 'TTL';
+$lang['RealtimeRegister.tab_dns.heading_priority'] = 'Priority';
+$lang['RealtimeRegister.tab_dns.heading_options'] = 'Options';
+$lang['RealtimeRegister.tab_dns.field_delete'] = 'Delete';
+$lang['RealtimeRegister.tab_dns.field_name'] = 'Name';
+$lang['RealtimeRegister.tab_dns.field_type'] = 'Type';
+$lang['RealtimeRegister.tab_dns.field_content'] = 'Content';
+$lang['RealtimeRegister.tab_dns.field_ttl'] = 'TTL';
+$lang['RealtimeRegister.tab_dns.field_prio'] = 'Priority';
+$lang['RealtimeRegister.tab_dns.field_add'] = 'Add Record';
+$lang['RealtimeRegister.tab_dns.text_no_dns_records'] = 'There are currently no DNS Records for this domain.';
+$lang['RealtimeRegister.tab_dns.text_dns_disabled'] = 'Internal DNS Zone has been disabled for this domain.';
+
+
+// Tab client DNS
+$lang['RealtimeRegister.tab_client_dns.title'] = 'DNS Zone';
+$lang['RealtimeRegister.tab_client_dns.heading'] = 'DNS Records';
+$lang['RealtimeRegister.tab_client_dns.heading_add_record'] = 'Add DNS Record';
+$lang['RealtimeRegister.tab_client_dns.heading_name'] = 'Name';
+$lang['RealtimeRegister.tab_client_dns.heading_type'] = 'Type';
+$lang['RealtimeRegister.tab_client_dns.heading_content'] = 'Content';
+$lang['RealtimeRegister.tab_client_dns.heading_ttl'] = 'TTL';
+$lang['RealtimeRegister.tab_client_dns.heading_priority'] = 'Priority';
+$lang['RealtimeRegister.tab_client_dns.heading_options'] = 'Options';
+$lang['RealtimeRegister.tab_client_dns.field_delete'] = 'Delete';
+$lang['RealtimeRegister.tab_client_dns.field_name'] = 'Name';
+$lang['RealtimeRegister.tab_client_dns.field_type'] = 'Type';
+$lang['RealtimeRegister.tab_client_dns.field_content'] = 'Content';
+$lang['RealtimeRegister.tab_client_dns.field_ttl'] = 'TTL';
+$lang['RealtimeRegister.tab_client_dns.field_prio'] = 'Priority';
+$lang['RealtimeRegister.tab_client_dns.field_add'] = 'Add Record';
+$lang['RealtimeRegister.tab_client_dns.text_no_dns_records'] = 'There are currently no DNS Records for this domain.';
+$lang['RealtimeRegister.tab_client_dns.text_dns_disabled'] = 'Internal DNS Zone has been disabled for this domain.';
+
+
+// Tab settings
+$lang['RealtimeRegister.tab_settings.title'] = 'Settings';
+$lang['RealtimeRegister.tab_settings.heading'] = 'Settings';
+$lang['RealtimeRegister.tab_settings.heading_dns'] = 'DNS Zone';
+$lang['RealtimeRegister.tab_settings.heading_auth_code'] = 'Authorization Code';
+$lang['RealtimeRegister.tab_settings.field_registrar_lock'] = 'Registrar Lock';
+$lang['RealtimeRegister.tab_settings.field_registrar_lock_yes'] = 'Set the registrar lock. Recommended to prevent unauthorized transfer.';
+$lang['RealtimeRegister.tab_settings.field_registrar_lock_no'] = 'Release the registrar lock so the domain can be transferred.';
+
+$lang['RealtimeRegister.tab_settings.field_enable_dns'] = 'Enable DNS Zone';
+$lang['RealtimeRegister.tab_settings.field_enable_dns_yes'] = 'Use the internal DNS zone.';
+$lang['RealtimeRegister.tab_settings.field_enable_dns_no'] = 'Use external Nameservers.';
+
+$lang['RealtimeRegister.tab_settings.field_request_epp'] = 'Request EPP Code/Transfer Key';
+$lang['RealtimeRegister.tab_settings.field_submit'] = 'Update Settings';
+$lang['RealtimeRegister.tab_settings.field_authcode'] = 'Authentication Code';
+$lang['RealtimeRegister.tab_settings.field_update_auth_code'] = 'Update Authentication Code';
+
+
+// Tab client settings
+$lang['RealtimeRegister.tab_client_settings.title'] = 'Settings';
+$lang['RealtimeRegister.tab_client_settings.heading'] = 'Settings';
+$lang['RealtimeRegister.tab_client_settings.heading_dns'] = 'DNS Zone';
+$lang['RealtimeRegister.tab_client_settings.heading_auth_code'] = 'Authorization Code';
+$lang['RealtimeRegister.tab_client_settings.field_registrar_lock'] = 'Registrar Lock';
+$lang['RealtimeRegister.tab_client_settings.field_registrar_lock_yes'] = 'Set the registrar lock. Recommended to prevent unauthorized transfer.';
+$lang['RealtimeRegister.tab_client_settings.field_registrar_lock_no'] = 'Release the registrar lock so the domain can be transferred.';
+
+$lang['RealtimeRegister.tab_client_settings.field_enable_dns'] = 'Enable DNS Zone';
+$lang['RealtimeRegister.tab_client_settings.field_enable_dns_yes'] = 'Use the internal DNS zone.';
+$lang['RealtimeRegister.tab_client_settings.field_enable_dns_no'] = 'Use external Nameservers.';
+
+$lang['RealtimeRegister.tab_client_settings.field_request_epp'] = 'Request EPP Code/Transfer Key';
+$lang['RealtimeRegister.tab_client_settings.field_submit'] = 'Update Settings';
+$lang['RealtimeRegister.tab_client_settings.field_authcode'] = 'Authentication Code';
+$lang['RealtimeRegister.tab_client_settings.field_update_auth_code'] = 'Update Authentication Code';
